f50b24c0b9713e2f0cd801236b2a0963a34c385a
Laura Royle <lauraamie.royle@gmail.com>
2019-02-04 20:08:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def adding
                   "oil paintings" => 3
                  }
 
-    shipping_manifest = ["muskets"] = "2", ["gun powder"] = "4"
+    shipping_manifest["muskets"] = 2, ["gun powder"] ="4"
 
     return shipping_manifest
 end
